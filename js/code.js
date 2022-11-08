533c59904436d22e87a57576a247eb5813e5cdf2
zimasile <jaydenngcukana@gmail.com>
2022-11-08 13:14:01 +0200

    first commit

---
+++ js/code.js
@@ -0,0 +1,20 @@
+let firstName = prompt("enter your name");
+let lastName = prompt("enter your surname");
+let salary = prompt("enter your salary");
+let bonus = 500;
+let product = eval(`${salary}+${bonus}`);
+
+document.write(`<br> NAME: ${firstName}`);
+document.write(`<br> SURNAME: ${lastName}`);
+document.write (`<br> SALARY: ${product}`);
+
+
+
+
+
+
+
+
+
+
+
